9ded7187d38c96c3d9f6eb007a0c7f4fc8614c26
Serg Dem <weblife999@gmail.com>
2024-09-25 02:15:06 +0500

    fix: fixed leak of key

--- src/createPerson.js
+++ src/createPerson.js
@@ -2,7 +2,7 @@ const pipedrive = require('pipedrive');
 
 async function addPerson() {
     const defaultClient = new pipedrive.ApiClient();
-    defaultClient.authentications.api_key.apiKey = '72aed2b4cc2a82b890b41bc7927901272211a255';
+    defaultClient.authentications.api_key.apiKey = 'YOUR_API_KEY';
 
     const api = new pipedrive.PersonsApi(defaultClient);
 
